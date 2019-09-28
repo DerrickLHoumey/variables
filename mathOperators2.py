ca08559d5aa4fd24d54b75fd1657932ba067bae0
DerrickLHoumey <55327123+DerrickLHoumey@users.noreply.github.com>
2019-09-28 12:25:15 -0500

    Create mathOperators2.py

---
+++ mathOperators2.py
@@ -0,0 +1,83 @@
+'''
+Created on Sep 28, 2019
+
+@author: ITAUser
+'''
+y = 2
+x = 9
+
+z = 2+9
+print(z)
+
+z = 3+6
+
+''' subtraction'''
+g= 1000
+h= 369
+
+f= g-h
+print(f)
+
+f = 9-3
+
+''' multiplication'''
+
+f= 3
+g= 6
+
+h= 3*6
+print(h)
+
+h=10*5
+
+'''division'''
+
+e= 5
+f= 5
+
+i= e/f
+print(i)
+
+f= 2/5
+
+'''double divide'''
+
+e= 10
+t= 15
+
+z= t//e
+
+print(e)
+
+f= 500//5
+
+'''modulus'''
+
+f= 33
+i= 99
+
+j= 99%33
+print(j)
+
+k= 42%4
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
