753c6f6e7ceda82fa0f77df166e586c476dbda62
DerrickLHoumey <55327123+DerrickLHoumey@users.noreply.github.com>
2019-10-05 10:18:08 -0500

    Create mathOperatorsDebug.py

---
+++ mathOperatorsDebug.py
@@ -0,0 +1,32 @@
+'''
+Created on Oct 5, 2019
+
+@author: ITAUser
+'''
+40 * 0 = -1  
+
+forty multiplied by zero is not negative one
+--------------------------------------
+“Hello 1 2 and” + 3 
+
+take out and, place 3 in the quatiation marks   
+--------------------------------------
+6 _5 _1= 31    
+
+6 * 5 + 1 = 31
+--------------------------------------
+print(10 + “/” + 2) = ?    
+
+remove the question mark and equal sign
+-------------------------------------- 
+print(“LOL” * 3)?        
+
+There should be no question mark
+--------------------------------------
+Print( “1” + “2” + “3”) 
+
+print should not be capitilized
+-------------------------------------
+print(10 _ 2 = 0) 
+
+modulus(%)       
